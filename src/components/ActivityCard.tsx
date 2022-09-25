681d50f83acce363945d9f16689b36aaa2ef1c39
AfteRRaIN-Sys <phet610@hotmail.com>
2022-09-26 00:01:29 +0700

    added logic to convert num to date locally

--- src/components/ActivityCard.tsx
+++ src/components/ActivityCard.tsx
@@ -8,6 +8,7 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 import "./ActivityCard.css"
+import { ListGroup, ListGroupItem } from "react-bootstrap";
 
 
 interface CardPropsObj  {
@@ -24,13 +25,17 @@ export default function ActivityCard({name, date, desc} : Activity) { //Activity
       <Card.Img variant="top" src="https://png.pngtree.com/thumb_back/fh260/background/20210902/pngtree-beautiful-sports-girls-beautiful-competition-photography-map-with-pictures-image_787591.jpg" />
       <Card.Body>
         <Card.Title>Event : {name}</Card.Title>
-        <Card.Text>
-          Description : {desc}
-        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        {/* <ListGroup.Item>{`Date : ${(new Date(date)).toUTCString()}`}</ListGroup.Item> */}
+        <ListGroup.Item>{`Date : ${(new Date(date)).toLocaleString()}`}</ListGroup.Item>
+        <ListGroup.Item>Description : {desc}</ListGroup.Item>
+      </ListGroup>
+      <Card.Footer>
         <div>
           <Button variant="success" onClick={()=>{alert("clicked")}}>Join</Button>
         </div>
-      </Card.Body>
+      </Card.Footer>
     </Card>
     </div>
   );
