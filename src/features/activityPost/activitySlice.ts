b659f9fee5c7f92d91b781eded416ff832c51c13
AfteRRaIN-Sys <phet610@hotmail.com>
2022-09-25 23:39:18 +0700

    added setState(action) logic successful

--- src/features/activityPost/activitySlice.ts
+++ src/features/activityPost/activitySlice.ts
@@ -1,17 +1,53 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Activity } from "../../models/activityTypes";
 
 export interface activitiesState {
   activities: Activity []
 }
 
+//sample
+const sampleCardData: Activity[] = [
+  {
+      name: "Badminton",
+      date: Date.now(),
+      desc: "Badminton_desc",
+  },
+  {
+      name: "Basketball",
+      date: Date.now(),
+      desc: "Basketball_desc",
+  },
+  {
+      name: "Basketball",
+      date: Date.now(),
+      desc: "Basketball_desc",
+  },
+];
+
 const initialState: activitiesState = {
-  activities: []
+  activities: sampleCardData
 };
 
 const activitySlice = createSlice({
   name: "activity",
   initialState,
-  reducers: {},
+  reducers: {
+    setActivities:(state, action: PayloadAction<Activity []>) => {
+      state.activities = [...action.payload];
+    }
+  },
   extraReducers: {}
 })
+
+// export const getAllActivities = createAsyncThunk(
+//   'counter/fetchCount',
+//   async (amount: number) => {
+//     const response = await fetchCount(amount);
+//     // The value we return becomes the `fulfilled` action payload
+//     return response.data;
+//   }
+// );
+
+export const { setActivities } = activitySlice.actions;
+
+export default activitySlice.reducer
