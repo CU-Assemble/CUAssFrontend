b3eaa7f4b9a7ac17e876dcb012c83c87822e4766
AfteRRaIN-Sys <phet610@hotmail.com>
2022-10-04 22:10:12 +0700

    added axios modules and services

--- src/components/ActivityPage.tsx
+++ src/components/ActivityPage.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import { Activity } from "../models/activityTypes";
+import AxiosModule from "../services/axiosModule";
+
 
 export default function ActivityPage({ ...props }: Activity) {
     console.log(props)
+    
     return (
         <Container>
             <h1>{props.name}</h1>
